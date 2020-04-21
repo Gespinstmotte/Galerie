06c2c460a80bb5c93aa3bd648a780449c7b67407
Gespinstmotte <64063469+Gespinstmotte@users.noreply.github.com>
2020-04-21 09:40:37 +0200

    Update gallerybest.js

--- assignment js/js/gallerybest.js
+++ assignment js/js/gallerybest.js
@@ -11,5 +11,5 @@ function unDo() {
 
     document.getElementById('image').style.backgroundImage = "url('')";
 
-    document.getElementById('image').innerHTML = "Hover over an image below to display here.";
+    document.getElementById('image').innerHTML = "Bewege die Maus über ein Bild.";
 }
